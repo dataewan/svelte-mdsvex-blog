e3f4364bf5aa24a0010bb1cbcb3eb90c63d07154
Ewan Nicolson <ewan.nicolson@gmail.com>
2021-03-09 18:07:34 +0000

    describing how I use the interactive components

---
+++ src/routes/blog/using-components/Counter.svelte
@@ -0,0 +1,10 @@
+<script>
+  export let count = 0;
+</script>
+
+
+<span>
+  <button on:click="{() => count = count -1 }">-</button>
+  {count}
+  <button on:click="{() => count = count +1 }">+</button>
+</span>
