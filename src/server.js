410ce87cbb88ecd1d4d58937a4bbe2beef33d3f9
Ewan Nicolson <ewan.nicolson@gmail.com>
2021-03-28 11:37:10 +0100

    script that pushes to github pages

--- src/server.js
+++ src/server.js
@@ -3,15 +3,17 @@ import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
 
-const { PORT, NODE_ENV } = process.env;
+const {PORT, NODE_ENV} = process.env;
 const dev = NODE_ENV === 'development';
+const url = dev ? '/' : "svelte-mdsvex-blog";
 
 polka() // You can also use Express
-	.use(
-		compression({ threshold: 0 }),
-		sirv('static', { dev }),
-		sapper.middleware()
-	)
-	.listen(PORT, err => {
-		if (err) console.log('error', err);
-	});
+  .use(
+    url,
+    compression({threshold: 0}),
+    sirv('static', {dev}),
+    sapper.middleware()
+  )
+  .listen(PORT, err => {
+    if (err) console.log('error', err);
+  });
